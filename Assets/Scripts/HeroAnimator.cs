115958045473d855bf632eff1b70bf40f654ae36
Nikolai <ne360cosmos@gmail.com>
2025-08-03 15:44:27 +0300

    Реализована механика передвижения главного героя

---
+++ Assets/Scripts/HeroAnimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class HeroAnimator : MonoBehaviour
+{
+    private static readonly int IsRunningKey = Animator.StringToHash("IsRunning");
+    private static readonly int VerticalVelocityKey = Animator.StringToHash("VerticalVelocity");
+    private static readonly int IsGroundedKey = Animator.StringToHash("IsGrounded");
+
+    [SerializeField] private LayerCheck _groundCheck;
+    
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 _direction;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetBool(IsRunningKey, _direction.x != 0);
+        _animator.SetFloat(VerticalVelocityKey, _rigidbody.velocity.y);
+        _animator.SetBool(IsGroundedKey, _groundCheck.IsTouchingLayer);
+    }
+
+    public void SetDirection(Vector2 direction) => 
+        _direction = direction;
+}
---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Mover))]
+[RequireComponent(typeof(HeroAnimator))]
+public class InputReader : MonoBehaviour
+{
+    private Mover _mover;
+    private HeroAnimator _heroAnimator;
+
+    private void Awake()
+    {
+        _mover = GetComponent<Mover>();
+        _heroAnimator = GetComponent<HeroAnimator>();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+        _mover.SetDirection(direction);
+        _heroAnimator.SetDirection(direction);
+    }
+}
